be6c1390a4f6153800b98eca8e89eb9b4191bcc6
dingky duo amurao <amurao.dingky.duo@asuene.com>
2026-01-14 13:51:16 +0800

    add Link in banner and handle graceFully if strapi is accesible

--- src/lib/API/homePageBanner.ts
+++ src/lib/API/homePageBanner.ts
@@ -3,8 +3,13 @@ import type { HomePageBanners } from "@/lib/type/catalog";
  
 
 export async function getHomeBanner(): Promise<HomePageBanners[]> {
-    const strapiUrl =  process.env.STRAPI_API_BASE_URL  
-    const result =  await fetchApi("/api/banners?populate=*" ,strapiUrl, false);
-    return result.data;
+    const strapiUrl =  process.env.STRAPI_API_BASE_URL 
+    try {
+        const result =  await fetchApi("/api/banners?populate=*" ,strapiUrl, false);
+        return result.data;
+    } catch (error) {
+        console.error("Error fetching home banners:", error);
+        return [];
+    }
 }
   
